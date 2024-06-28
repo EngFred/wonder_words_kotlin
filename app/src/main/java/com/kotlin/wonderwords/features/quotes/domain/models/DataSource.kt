0969bc7edc4747918b523721aa95d6debdb38edc
EngFred <engfred88@gmail.com>
2024-06-28 15:30:25 +0300

    Custom pagination and caching implementation, share quote implementation

--- app/src/main/java/com/kotlin/wonderwords/features/quotes/domain/models/DataSource.kt
+++ app/src/main/java/com/kotlin/wonderwords/features/quotes/domain/models/DataSource.kt
@@ -1,7 +1,5 @@
 package com.kotlin.wonderwords.features.quotes.domain.models
 
-import com.kotlin.wonderwords.features.quotes.data.repository.Source
-
 data class DataSource(
     val source: Source,
     val data: List<Quote>
